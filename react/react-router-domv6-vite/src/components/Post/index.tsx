9fad9ff7893fa6e434ab24dc64708a04da47d3e1
Ivan <ivan.reisrosa@gmail.com>
2022-12-21 20:02:56 -0300

    Outlet

--- react/react-router-domv6-vite/src/components/Post/index.tsx
+++ react/react-router-domv6-vite/src/components/Post/index.tsx
@@ -1,16 +1,15 @@
-import { useParams, useSearchParams } from "react-router-dom";
 import "./style.css";
 
 export const Post = () => {
-  const params = useParams();
-  const { id } = params;
-  const [qs] = useSearchParams();
-
   return (
     <div>
-      <h1>
-        Post {`Param: ${id}`} {`QS: ${qs.get("page")}`}
-      </h1>
+      <h1>Post</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam eaque
+        repellendus, laudantium voluptatem libero, soluta nihil consectetur
+        doloremque necessitatibus ex quae ullam aperiam iste error, assumenda
+        corporis magnam excepturi omnis?
+      </p>
     </div>
   );
 };
