6b65741782e8c3e68d0d224602e41ac3c77a5f1d
Ivan <ivan.reisrosa@gmail.com>
2022-12-26 17:47:28 -0300

    Hooks - createContext, useContext

--- react/quiz/src/components/Welcome.jsx
+++ react/quiz/src/components/Welcome.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import { QuizContext, QuizProvider } from "../context/quiz";
+
 import quiz from "../img/quiz.svg";
 
 import "./Welcome.css";
 
 const Welcome = () => {
+  const quizState = useContext(QuizContext);
+
+  console.log(quizState);
+
   return (
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
